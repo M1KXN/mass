a605bfb111de5f3342bf50ec7e16ea38c4cb752c
Mikhail Kropachev <mishakro@yandex.ru>
2024-06-08 14:31:08 +0300

    visible

--- 2zadanie.pas
+++ 2zadanie.pas
@@ -20,32 +20,32 @@ begin
     for i:= 0 to 9 do
     writeln('m [', i, '] = ', m[i]);
 end.}
+
 const n = 9;
 var
     m : array[0..n] of integer;
     i,j, buf, c: integer;
 begin
     for i:=0 to n do
-    begin
-        m[i] := Random(10) + 1;
-        writeln('m [', i, '] = ', m[i]);
-    end;
+        begin
+            m[i] := Random(10) + 1;
+            writeln('m [', i, '] = ', m[i]);
+        end;
     for i:= 0 to n do
-    begin
-    buf := i;
-    for j := i+1 to n do
-    if m[j] < m[buf] then
-    buf := j;
-    if buf <> i then
-    begin
-    c := m[buf];
-    m[buf] := m[i];
-    m[i] := c;
-    end;
-    end;
+        begin
+            buf := i;
+            for j := i+1 to n do
+                if m[j] < m[buf] then
+                    buf := j;
+            if buf <> i then
+                begin
+                    c := m[buf];
+                    m[buf] := m[i];
+                    m[i] := c;
+                end;
+        end;
 
-
-writeln;    
+writeln;
     for i:= 0 to 9 do
-    writeln('m [', i, '] = ', m[i]);
+        writeln('m [', i, '] = ', m[i]);
 end.
